f56d59bdc120f5f9d34b313cee94c1ee118db867
Puria Nafisi Azizi <puria@dyne.org>
2021-08-07 14:26:24 +0200

    Fix naming prefix for family

--- src/handler/handler.rs
+++ src/handler/handler.rs
@@ -58,7 +58,7 @@ trait SwTransactions {
 impl SwTransactionHandler {
     pub fn new() -> SwTransactionHandler {
         SwTransactionHandler {
-            family_name: String::from("restroom"),
+            family_name: String::from("wallet"),
             family_versions: vec![String::from("1.0")],
             namespaces: vec![String::from(get_sw_prefix().to_string())],
         }
